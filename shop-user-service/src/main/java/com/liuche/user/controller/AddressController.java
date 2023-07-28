eb63ec6713c7c2771f3c2ac36ee1e224a169aa81
liuche <706716852@qq.com>
2023-07-28 20:10:11 +0800

    2-5 新增了删除和查询指定地址信息接口

--- shop-user-service/src/main/java/com/liuche/user/controller/AddressController.java
+++ shop-user-service/src/main/java/com/liuche/user/controller/AddressController.java
@@ -1,16 +1,23 @@
 package com.liuche.user.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.liuche.common.util.CopyUtil;
 import com.liuche.common.util.JsonData;
+import com.liuche.common.util.RequestContext;
+import com.liuche.user.model.Address;
 import com.liuche.user.model.request.AddressAddRequest;
+import com.liuche.user.model.response.AddressInfoResp;
 import com.liuche.user.service.AddressService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author 刘彻
@@ -22,9 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/address")
 @Slf4j
-@ApiOperation(value = "地址相关接口")
 public class AddressController {
-    @Autowired
+    @Resource
     private AddressService addressService;
 
     @PostMapping("/save/address")
@@ -34,6 +40,36 @@ public class AddressController {
             return JsonData.error("参数错误");
         }
         int i = addressService.addAddress(request);
-        return i==1?JsonData.ok("成功添加一个收获地址"):JsonData.error("添加错误");
+        return i == 1 ? JsonData.ok("成功添加一个收获地址") : JsonData.error("添加错误");
+    }
+
+    @GetMapping("/get-pne/address/{id}")
+    @ApiOperation(value = "查询指定的收货地址")
+    public JsonData getOneAddress(@PathVariable @ApiParam(value = "需要查询的地址ID") String id) {
+        Address address = addressService.getOne(new QueryWrapper<Address>()
+                .eq("user_id", RequestContext.getUserId())
+                .eq("id", Long.parseLong(id)));
+        AddressInfoResp infoResp = CopyUtil.copy(address, AddressInfoResp.class);
+        return address == null ? JsonData.error("未查到") : JsonData.ok(infoResp);
+    }
+
+    @PostMapping("/list/address")
+    @ApiOperation(value = "查询该用户所有的收货地址")
+    public JsonData addressList() {
+        // 直接返回该用户下所有的地址
+        List<Address> list = addressService.list();
+        // 脱敏
+        List<AddressInfoResp> respList = CopyUtil.copyList(list, AddressInfoResp.class);
+        return JsonData.ok(respList);
+    }
+
+    @GetMapping("/del/address/{id}")
+    @ApiOperation(value = "删除指定的收货地址")
+    public JsonData deleteAddress(@PathVariable @ApiParam(value = "需要删除的地址") String id) {
+        // 删除指定地址
+        boolean flag = addressService.remove(new QueryWrapper<Address>()
+                .eq("user_id", RequestContext.getUserId())
+                .eq("id", Long.parseLong(id)));
+        return flag ? JsonData.ok("删除成功！") : JsonData.error("删除失败");
     }
 }
