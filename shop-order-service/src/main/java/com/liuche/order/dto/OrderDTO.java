e4bd77bc2464e7852db8757e5bd90380918625f1
liuche <706716852@qq.com>
2023-08-03 12:28:05 +0800

    5-3 dto类的默认参数配置配错了进行了修改

--- shop-order-service/src/main/java/com/liuche/order/dto/OrderDTO.java
+++ shop-order-service/src/main/java/com/liuche/order/dto/OrderDTO.java
@@ -1,6 +1,7 @@
 package com.liuche.order.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -19,48 +20,48 @@ public class OrderDTO {
     /**
      * NEW 未支付订单,PAY已经支付订单,CANCEL超时取消订单
      */
-    @ApiParam(value = "订单状态",example = "NEW")
+    @ApiModelProperty(value = "订单状态",example = "NEW")
     private String state;
 
     /**
      * 订单总金额
      */
-    @ApiParam(value = "订单总金额",example = "520")
+    @ApiModelProperty(value = "订单总金额",example = "520")
     private BigDecimal totalAmount;
 
     /**
      * 订单实际支付价格
      */
-    @ApiParam(value = "订单实际支付价格",example = "520")
+    @ApiModelProperty(value = "订单实际支付价格",example = "520")
     private BigDecimal payAmount;
 
     /**
      * 支付类型，微信-银行-支付宝
      */
-    @ApiParam(value = "支付类型，微信-银行-支付宝",example = "WECHAT")
+    @ApiModelProperty(value = "支付类型，微信-银行-支付宝",example = "WECHAT")
     private String payType;
 
     /**
      * 昵称
      */
-    @ApiParam(value = "昵称",example = "大大怪")
+    @ApiModelProperty(value = "昵称",example = "大大怪")
     private String nickname;
 
     /**
      * 头像
      */
-    @ApiParam(value = "头像",example = "https://file.xdclass.net/video/2020/alibabacloud/zt-alibabacloud.png")
+    @ApiModelProperty(value = "头像",example = "https://file.xdclass.net/video/2020/alibabacloud/zt-alibabacloud.png")
     private String headImg;
 
     /**
      * 订单类型 DAILY普通单，PROMOTION促销订单
      */
-    @ApiParam(value = "订单类型 DAILY普通单，PROMOTION促销订单",example = "PROMOTION")
+    @ApiModelProperty(value = "订单类型 DAILY普通单，PROMOTION促销订单",example = "PROMOTION")
     private String orderType;
 
     /**
      * 收货地址json存储
      */
-    @ApiParam(value = "收货地址json存储",example = "火星")
+    @ApiModelProperty(value = "收货地址json存储",example = "火星")
     private String receiverAddress;
 }
